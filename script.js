65a8fe03c53b674acc5bf863cd5ca3626c6fa242
Colby Scarbrough <cws18472@gmail.com>
2020-10-04 19:31:47 -0400

    added

---
+++ script.js
@@ -0,0 +1,18 @@
+/**********************************************
+
+This script is not for the actual Carbon calculator
+script works with functionality of the web application
+
+UI of website
+
+Authors : Colby Scarbrough
+
+***********************************************/
+
+
+
+function main() {
+    return null;
+}
+
+main();
