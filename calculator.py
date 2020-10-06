a11a5c80e1d946a8515c1f2f22a1a5d66578fd9c
maisha iqbal <mi4404@rit.edu>
2020-10-05 23:35:58 -0400

    added householdmembers,housesize, meatchoices

---
+++ calculator.py
@@ -0,0 +1,78 @@
+import switch as switch
+
+
+def householdmembers() -> int:
+    """
+    Asks how many household members you have. Then gives you points accordingly.
+    :return: int
+    """
+
+    #asks how many household members
+    householdmembers = input("""How many household members do you have (including you)? Choose a number! 
+    1. 1
+    2. 2
+    3. 3
+    4. 4
+    5. 5
+    6. more than 5?\n""")
+
+
+    #change this to a switch statement
+    #this adds points to the carbon footprint
+
+    #this does not account for wrong input
+    return 16 - int(householdmembers)*2
+
+def housesize() -> int:
+    """
+    Asks how big your house is. Then gives you points accordingly.
+    :return: int
+    """
+
+    #asks big house is
+    housesize = input("""How big is your home? Choose a number! 
+    1. Large( less than 5000 sqft )
+    2. Medium ( less than 1500 sqft )
+    3. Small ( less than 500 sqft )
+    4. Apartment ( less than 150 sqft )\n""")
+
+
+    #change this to a switch statement
+    #this adds points to the carbon footprint
+
+    #this does not account for wrong input
+    return 13 - int(housesize)*3
+
+def meatchoices() -> int:
+    """
+    Asks about your food choices. Then gives you points accordingly.
+    :return: int
+    """
+
+    #asks about your food choices
+    meatchoices = input("""How often do you eat domestic meat per week? Choose a number! 
+    1. Daily
+    2. Few times
+    3. Vegetarian
+    4. Vegan\n""")
+
+
+    #this adds points to the carbon footprint
+
+    #this does not account for wrong input
+    return 12 - int(meatchoices)*2
+
+def main() -> None:
+    """
+    The main function.
+    :return: None
+    """
+    #these calculations were taken from https://www.wikihow.com/Calculate-Your-Carbon-Footprint for now
+
+    points = 0;
+    points += householdmembers()
+    points += housesize()
+    points += meatchoices()
+    print(points)
+if __name__ == '__main__':
+    main()
