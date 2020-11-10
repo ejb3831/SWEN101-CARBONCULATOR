3dd7d6bded98049f7069781cc829946cb7c27141
maisha iqbal <mi4404@rit.edu>
2020-11-10 15:20:40 -0500

    added results

--- improvedcalculator.py
+++ improvedcalculator.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def quiz(answers: list) -> int:
+def quiz(answers: list, results: list) -> int:
     #these calculations were taken from https://www.wikihow.com/Calculate-Your-Carbon-Footprint for now
 
     points = 0;
@@ -14,12 +14,21 @@ def quiz(answers: list) -> int:
 
     #meat choice
     points += 12 - int(answers[2])*2
+    if(answers[2]<2):
+        results.append('You can help by eating less meat. '
+                       'https://www.greenpeace.org/usa/sustainable-agriculture/eco-farming/eat-more-plants/')
 
     #dish washer
     points += int(answers[3]) - 1
+    if(answers[3]>2):
+        results.append('You can help by using less water.' 
+                        'https://www.epa.gov/watersense/how-we-use-water')
 
     #washing machine
     points += int(answers[4]) - 1
+    if(answers[4]>2):
+        results.append('You can help by using less water.' 
+                        'https://www.epa.gov/watersense/how-we-use-water')
 
     #household purchases
     points += 12 - int(answers[5])*2
@@ -30,12 +39,23 @@ def quiz(answers: list) -> int:
     else:
         points +=int(answers[6])*10
 
+    if(answers[6]>2):
+        results.append('You can help by producing less waste.' 
+                        'https://www.earthday.org/how-our-trash-impacts-the-environment/')
+
     #Recycle
     points += 24 - int(answers[7] - 1)*4
+    if(answers[7]<4):
+        results.append('You can help by recycling more' 
+                        'https://www.epa.gov/recycle/recycling-basics')
 
     #personal transportation
     if(answers[8]!=5):
         points +=14 - int(answers[8])*2
+
+    if(answers[8]<=2):
+        results.append('You can help by taking public transportation and carpooling.' 
+                        'https://www.greenamerica.org/green-living/carpool-climate-and-community')
 
     #public transportation
     if(answers[9] != 6):
@@ -48,6 +68,10 @@ def quiz(answers: list) -> int:
         else:
             points +=  14 - int(answers[9]) * 2
 
+    if(answers[9]>2):
+        results.append('You can help by using public transportation or using it more often than other ways.'
+                       'https://www.apta.com/news-publications/public-transportation-benefits/')
+
     #flights
     if(answers[10] !=4):
         if(int(answers[10])==1):
@@ -59,15 +83,17 @@ def quiz(answers: list) -> int:
 
     return points
 
+
 def main() -> None:
     """
     The main function.
     :return: None
     """
     answers = list()
+    results = list()
     sys.args[0] = answers
 
-    quizpoints = quiz(answers)
+    quizpoints = quiz(answers, results)
 
 if __name__ == '__main__':
     main()
